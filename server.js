043c5c55ba942deec499785e48ea9c13583624e4
damian1997 <d.veltkamp137@gmail.com>
2021-03-11 19:52:27 +0100

    Setup eslint and fixed errors eslint was giving me

--- server.js
+++ server.js
@@ -3,21 +3,20 @@ import nunjucks from 'nunjucks';
 import compression from 'compression';
 import dotenv from 'dotenv';
 import router from './routes/index.routes';
-import fs from 'fs';
+import * as defaultHelpers from './helpers/default.helpers';
 
 dotenv.config();
 
-const port = process.env.PORT || 3000,
-  environment = process.env.ENVIRONMENT || 'development',
-  urlEncodedParser = express.urlencoded({ extended: true }),
-  app = express();
+const port = process.env.PORT || 3000;
+const urlEncodedParser = express.urlencoded({ extended: true });
+const app = express();
 
 nunjucks
-  .configure(['source/views', ...getComponentPaths()], {
+  .configure(['source/views', ...defaultHelpers.getComponentPaths()], {
     autoescape: true,
     express: app,
   })
-  .addGlobal('cssBundle', getCssBundleName());
+  .addGlobal('cssBundle', defaultHelpers.getCssBundleName());
 
 app
   .use(compression())
@@ -27,19 +26,3 @@ app
   .set('view engine', 'html')
   .use('/', router)
   .listen(port, () => console.log(`Using port: ${port}`));
-
-function getComponentPaths() {
-  const componentsPath = 'source/components';
-  const components = fs.readdirSync(componentsPath);
-
-  const templatePaths = components.map((component) => {
-    return `${componentsPath}/${component}/template/`;
-  });
-
-  return templatePaths;
-}
-
-function getCssBundleName() {
-  const cssBundle = fs.readdirSync('static/build/css/');
-  return cssBundle[0];
-}
